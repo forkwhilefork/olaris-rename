49d423be860f917eda32b678846b646769ea3f08
Maran <maran@protonmail.com>
2019-11-20 11:17:41 +0100

    Scan recursively and lookup on TMDB by default

--- flags.go
+++ flags.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 )
 
-var recursive = flag.Bool("recursive", false, "Scan folders inside of other folders.")
+var recursive = flag.Bool("recursive", true, "Scan folders inside of other folders.")
 var logToFile = flag.Bool("log-to-file", false, "Logs are written to stdout as well as a logfile.")
 var verbose = flag.Bool("verbose", false, "Show debug log information.")
 var dryrun = flag.Bool("dry-run", false, "Don't actually modify any files.")
@@ -13,5 +13,5 @@ var filePath = flag.String("filepath", "", "Path to scan (can be a folder or fil
 var movieFolder = flag.String("movie-folder", defaultMovieFolder(), "Folder where movies should be placed")
 var seriesFolder = flag.String("series-folder", defaultSeriesFolder(), "Folder where series should be placed")
 var musicFolder = flag.String("music-folder", defaultMusicFolder(), "Folder where music should be placed")
-var tmdbLookup = flag.Bool("tmdb-lookup", false, "Should the TMDB be used for better look-up and matching")
+var tmdbLookup = flag.Bool("tmdb-lookup", true, "Should the TMDB be used for better look-up and matching")
 var extractPath = flag.String("extract-path", defaultExtractedFolder(), "Path to extract content to.")

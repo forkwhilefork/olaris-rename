5580d27166fd2259a7b30853bb1c3ae342420322
Animazing <animazing@gmail.com>
2022-05-06 13:11:02 +0000

    Rewrite to use Olaris-Rename as a Library from other applications

--- identify/file.go
+++ identify/file.go
@@ -1,9 +1,8 @@
-package main
+package identify
 
 import (
 	"fmt"
 
-	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -33,6 +32,11 @@ type ParsedFile struct {
 	IsMusic      bool
 	ExternalID   int
 	OriginalFile string
+	Options      Options
+}
+
+func (p *ParsedFile) String() string {
+	return fmt.Sprintf("Year: %s, Season: %s, Episode: %s, Name: %s, Movie: %v, Series: %v", p.Year, p.Season, p.Episode, p.CleanName, p.IsMovie, p.IsSeries)
 }
 
 type Options struct {
@@ -40,36 +44,66 @@ type Options struct {
 	ForceMovie   bool
 	ForceSeries  bool
 	OriginalFile string
+	MovieFormat  string
+	SeriesFormat string
+	DryRun       bool
+}
+
+func (p *Options) String() string {
+	return fmt.Sprintf("Lookup: %v, ForceMovie: %v, ForceSeries: %v, OriginalFile: %s, MovieFormat: %s, SeriesFormat: %s, DryRun: %v", p.Lookup, p.ForceMovie, p.ForceSeries, p.OriginalFile, p.MovieFormat, p.SeriesFormat, p.DryRun)
+}
+
+func GetDefaultOptions() Options {
+	return getOpts([]Options{})
 }
 
 func getOpts(o []Options) (opts Options) {
 	if len(o) == 0 {
-		opts = Options{Lookup: false}
+		opts = Options{}
 	} else {
 		opts = o[0]
+	}
+	if opts.MovieFormat == "" {
+		opts.MovieFormat = DefaultMovieFormat
+	}
+	if opts.SeriesFormat == "" {
+		opts.SeriesFormat = DefaultSeriesFormat
 	}
 	return opts
 }
 
 func NewParsedFile(filePath string, o ...Options) ParsedFile {
 	opts := getOpts(o)
-
-	f := ParsedFile{Filepath: filePath, OriginalFile: opts.OriginalFile}
+	log.WithField("options", opts.String()).Debugln("Parsing filename with options")
+	f := ParsedFile{Filepath: filePath, OriginalFile: opts.OriginalFile, Options: opts}
 	f.Extension = filepath.Ext(filePath)
 	filename := strings.TrimSuffix(filePath, f.Extension)
 	filename = filepath.Base(filename)
 	f.Filename = filename
 	log.WithFields(log.Fields{"file": f.Filename}).Debugln("Checking file")
 
-	if supportedVideoExtensions[f.Extension] {
+	if SupportedVideoExtensions[f.Extension] {
 		for _, match := range order {
 			res := matchers[match].FindStringSubmatch(filename)
 			if len(res) > 0 {
 				switch match {
+				case "yearAsSeason":
+					if len(res) > 1 {
+						log.WithField("year", res[2]).Debugln("Found year as season.")
+						f.Season = res[2]
+					}
 				case "year":
-					f.Year = res[2]
+					if f.Season == res[2] {
+						log.Warnln("We found a year that is the same as the season, to prevent issues with looking up the wrong year we are ignoring the found year.")
+					} else {
+						f.Year = res[2]
+					}
 				case "season":
-					f.Season = fmt.Sprintf("%02s", res[2])
+					if f.Season == "" {
+						f.Season = fmt.Sprintf("%02s", res[2])
+					} else {
+						log.Debugln("We already have found a season earlier so skipping the normal season match.")
+					}
 				case "episode":
 					f.Episode = fmt.Sprintf("%02s", res[1])
 				case "quality":
@@ -105,6 +139,7 @@ func NewParsedFile(filePath string, o ...Options) ParsedFile {
 					return NewParsedFile(fileParent+f.Extension, opts)
 				}
 			}
+			log.Debugln("Starting actual matching.")
 
 			for _, match := range order {
 				res := matchers[match].FindStringSubmatch(cleanName)
@@ -118,7 +153,7 @@ func NewParsedFile(filePath string, o ...Options) ParsedFile {
 						} else if match == "groupAnime" {
 							cleanName = strings.Replace(cleanName, res[1], " ", -1)
 						} else {
-							cleanName = matchers[match].ReplaceAllString(cleanName, "")
+							cleanName = matchers[match].ReplaceAllString(cleanName, " ")
 						}
 					}
 				}
@@ -128,6 +163,7 @@ func NewParsedFile(filePath string, o ...Options) ParsedFile {
 
 			// Anime content is really weird, if we do this we might kill the name completely
 			if f.AnimeGroup == "" {
+				log.WithField("cleanName", cleanName).Debugln("Probably not Anime so cleaning a bit more.")
 				cleanName = regexp.MustCompile(`\s{2,}.*`).ReplaceAllString(cleanName, "")
 				cleanName = strings.Trim(cleanName, " -")
 				cleanName = cases.Title(language.English).String(cleanName)
@@ -135,8 +171,9 @@ func NewParsedFile(filePath string, o ...Options) ParsedFile {
 		}
 
 		cleanName = strings.Replace(cleanName, ":", "", -1)
+
 		f.CleanName = cleanName
-	} else if supportedMusicExtensions[f.Extension] {
+	} else if SupportedMusicExtensions[f.Extension] {
 		f.IsMusic = true
 		return f
 	} else {
@@ -148,20 +185,20 @@ func NewParsedFile(filePath string, o ...Options) ParsedFile {
 	}
 
 	if addYearToSeries[f.CleanName] && f.Year != "" {
-		log.WithFields(log.Fields{"year": f.Year, "name": f.CleanName}).Debugln("Found series which requires year to be added")
+		log.WithFields(log.Fields{"year": f.Year, "name": f.CleanName}).Debugln("Found seriesname that has multiple series with the same name but different years so adding the year into the final name.")
 		f.CleanName = fmt.Sprintf("%s (%s)", f.CleanName, f.Year)
 	}
 
 	// Windows really hates colons, so lets strip them out.
 	f.CleanName = strings.Replace(f.CleanName, ":", "", -1)
 
+	log.WithField("cleanName", f.String()).Debugln("Final result.")
+
 	return f
 }
 
 // SourcePath returns the originfile if one is available otherwise does the most recursed hit.
 func (p *ParsedFile) SourcePath() string {
-	log.Println(p.Filepath)
-	log.Println(p.OriginalFile)
 	if p.OriginalFile == "" {
 		return p.Filepath
 	} else {
@@ -188,7 +225,6 @@ func queryTmdb(p *ParsedFile) error {
 	}
 
 	log.WithFields(log.Fields{"year": p.Year, "title": p.CleanName}).Debugln("Trying to locate data from TMDB")
-
 	if p.IsSeries {
 		searchRes, err := agent.SearchTv(p.CleanName, options)
 		if err != nil {
@@ -198,6 +234,7 @@ func queryTmdb(p *ParsedFile) error {
 
 		if len(searchRes.Results) > 0 {
 			tv := searchRes.Results[0] // Take the first result for now
+			log.Debugln("TV:", tv)
 			p.ExternalID = tv.ID
 			p.ExternalName = tv.Name
 			p.CleanName = tv.Name
@@ -205,7 +242,7 @@ func queryTmdb(p *ParsedFile) error {
 				p.Year = strings.Split(tv.FirstAirDate, "-")[0]
 			}
 		} else {
-			log.Debugln("No results")
+			log.Debugln("No results found on TMDB")
 		}
 
 	} else if p.IsMovie {
@@ -216,14 +253,20 @@ func queryTmdb(p *ParsedFile) error {
 		}
 
 		if len(searchRes.Results) > 0 {
+
 			mov := searchRes.Results[0] // Take the first result for now
+			log.Debugln("Movie:", mov)
+
 			p.ExternalID = mov.ID
 			p.ExternalName = mov.Title
 			p.CleanName = mov.Title
+
 		} else {
-			log.Debugln("No results")
+			log.Debugln("No results found on TMDB")
 		}
 	}
+
+	log.WithFields(log.Fields{"externalID": p.ExternalID, "externalName": p.ExternalName}).Debugln("Received TMDB results.")
 
 	return nil
 }
@@ -233,9 +276,9 @@ func (p *ParsedFile) TargetName() string {
 	var newName string
 
 	if p.IsMovie {
-		newName = *movieFormat
+		newName = p.Options.MovieFormat
 	} else if p.IsSeries {
-		newName = *seriesFormat
+		newName = p.Options.SeriesFormat
 		newName = strings.Replace(newName, "{s}", p.Season, -1)
 		newName = strings.Replace(newName, "{e}", p.Episode, -1)
 	} else {
@@ -249,74 +292,16 @@ func (p *ParsedFile) TargetName() string {
 	newName = strings.Replace(newName, "{y}", p.Year, -1)
 	newName = strings.Trim(newName, " ")
 
+	// Sometimes we end up with an extra dot somehow. This should remove the extra dot
+	// I'm tired, this can probably be done better...
+	if (string(newName[len(newName)-1])) == "." {
+		newName = newName[0 : len(newName)-1]
+	}
+
 	return newName + p.Extension
 }
 
 // FullName is the original name of the file without the ful path
 func (p *ParsedFile) FullName() string {
 	return p.Filename + p.Extension
-}
-
-func (p ParsedFile) Act(targetFolder, action string) error {
-	source, err := filepath.Abs(p.SourcePath())
-	if err != nil {
-		return err
-	}
-
-	targetLocation := filepath.Join(targetFolder, p.TargetName())
-
-	if !*dryrun {
-		err = ensurePath(filepath.Dir(targetLocation))
-		if err != nil {
-			return err
-		}
-
-		log.WithFields(log.Fields{"target": targetLocation, "source": source, "action": action}).Infoln("Acting on file")
-		if _, err := os.Lstat(targetLocation); err == nil {
-			log.Warnln("File already exists, doing nothing.")
-			return nil
-		}
-
-		if action == "symlink" {
-			source, err = filepath.EvalSymlinks(source)
-			if err != nil {
-				log.WithFields(log.Fields{"targetLocation": filepath.Dir(targetLocation), "source": source, "err": err}).Debugln("error during symlink evaluation")
-				return err
-			}
-
-			log.WithFields(log.Fields{"source": source, "target": targetLocation}).Debugln("Evaling symlinks")
-			source, err = filepath.Rel(filepath.Dir(targetLocation), source)
-
-			if err != nil {
-				log.WithFields(log.Fields{"targetLocation": filepath.Dir(targetLocation), "source": source, "err": err}).Debugln("error during Rel call")
-				return err
-			}
-
-			log.WithFields(log.Fields{"target": targetLocation, "source": source, "action": action}).Infoln("Using relative path for symlinks.")
-
-			err = os.Symlink(source, targetLocation)
-			if err != nil {
-				return err
-			}
-		} else if action == "hardlink" {
-			err = os.Link(source, targetLocation)
-			if err != nil {
-				return err
-			}
-		} else if action == "copy" {
-			err := copyFileContents(source, targetLocation)
-			if err != nil {
-				return err
-			}
-		} else if action == "move" {
-			err := os.Rename(source, targetLocation)
-			if err != nil {
-				return err
-			}
-		}
-	} else {
-		log.WithFields(log.Fields{"target": targetLocation, "source": source, "action": action}).Infoln("--dry-run enabled, not acting on file")
-	}
-
-	return nil
 }
